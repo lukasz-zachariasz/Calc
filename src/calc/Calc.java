108788a227d32dbb0233fc6ddc42b71ce7f9cce9
lukasz-zachariasz <lukas.zachi@gmail.com>
2014-04-12 03:35:17 +0200

    Poteżna zmiana w głównej klasie która odmieni losy wszechświata

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -2,7 +2,8 @@ package calc;
 public class Calc {
     public static void main(String[] args) {
         //TODO:EVERTHING KURWA MAC
-        
+        String[]as = null;
+        Okon.main(as);
     }
     
 }
